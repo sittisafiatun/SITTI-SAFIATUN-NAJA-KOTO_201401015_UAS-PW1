b64de6f64af92cfbe1bec2c37d7e3cb87699a783
sittisafiatun <safiatunsitti2020@gmail.com>
2021-06-09 15:54:14 +0700

    upload New files

---
+++ footer.php
@@ -0,0 +1,8 @@
+<footer class="footer mt-5 mb-5 text-center" id="hak">
+                 <img src="logo-Lombok.jpg"  height="66px" class="mr-3"> LOMBOK  <hr> Tempat Wisata &copy;2021
+            </footer>
+            <!--awal footer-->
+
+        </body>
+    </html>
+
---
+++ index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<?php include('header.php');?>
+  
+            <!--akhir navbar-->
+
+            <!--awal Utama-->
+            <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                  <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                  <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                  <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                  <div class="carousel-item active">
+                    <img src="Gbr 1.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption">
+                      <h1>Air Terjun Penimbung</h1>
+                      <p>Air Terjun Penimbung memiliki keunikan yang istimewa, yaitu menyuguhkan panorama alam yang luar biasa.</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="Gbr 2.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption">
+                      <h1>Gunung Rinjani</h1>
+                      <p>Tempat terbaik untuk melihat matahari terbenam di Lombok.</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="Gbr 3.png" class="d-block w-100" alt="...">
+                    <div class="carousel-caption">
+                      <h1>Gua Bangkang</h1>
+                      <p>Gua Bangkang dengan spot wisata yang eksotis .</p>
+                    </div>
+                  </div>
+                  <div class="carousel-item">
+                    <img src="Gbr 4.jpg" class="d-block w-100" alt="...">
+                    <div class="carousel-caption">
+                      <h1>Gili Trawangan</h1>
+                      <p>Gili Trawangan memiliki pantai dan alam bawah laut yang sangat indah.</p>
+                    </div>
+                  </div>
+                </div>
+                <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                  <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                  <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </div>
+            <!--akhir Utama-->
+
+
+
+            <!--awal Jadwal-->
+            <div class="jumbotron" id="Jadwal">
+                <div class="container">
+                <h1 class="display-4">Jadwal Wisata</h1>
+                <table class="table table-striped">
+                    <thead>
+                        <th>Waktu</th>
+                        <th>Sabtu</th>
+                        <th>Minggu</th>
+                    </thead>
+                    <tr>
+                        <th>07.00 - 10.40</th>
+                        <td>Air terjun Penimbung</td>
+                        <td>Gua Bangkang</td>
+                    </tr>
+                    <tr>
+                        <th>10.40 - 13.20</th>
+                        <td>Perjalanan</td>
+                        <td>Perjalanan</td>
+                    </tr>
+                    <tr>
+                        <th>13.20 - 19.00</th>
+                        <td>Gunung Rinjani</td>
+                        <td>Gili Trawangan</td>
+                    </tr>
+                </table>
+              </div>  
+              </div>
+            <!--akhir Jadwal-->
+
+            <!--awal card-->
+            <div class="container-fluid mt-5" id="fasilitas">
+            <div class="row">
+            <div class="col-lg-3 col-md-6">
+            <div class="card">
+              <img src="Bus.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Bus Pariwisata</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="https://www.pagitrans.com/sewa-bus-di-lombok/" class="btn btn-primary">Klik</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="card">
+              <img src="hotel.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Hotel</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="https://www.booking.com/region/id/lombok.id.html" class="btn btn-primary">Klik</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="card">
+              <img src="makanan.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Makanan</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="https://travelingyuk.com/makanan-khas-lombok/150180/" class="btn btn-primary">Klik</a>
+              </div>
+            </div>
+          </div>
+
+          <div class="col-lg-3 col-md-6">
+            <div class="card">
+              <img src="Dokumentasi.jpg" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 class="card-title">Dokumentasi berupa foto atau video</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="https://neximage.com/tips-foto-dokumentasi/" class="btn btn-primary">Klik</a>
+              </div>
+          </div>
+            </div>
+            </div>
+              </div>
+            <!--akhir card-->
+
+            <!--awal peta-->
+            <div class="embed-responsive embed-responsive-16by9 mt-5" id="lokasi">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1009955.3724517224!2d115.71251204139816!3d-8.582710994089279!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dcdb7d23e8cc745%3A0x446689c4ab50d8c9!2sPulau%20Lombok!5e0!3m2!1sid!2sid!4v1619156809033!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+            </div>
+            <!--akhir peta-->
+            <div class="col-lg-12 col-md-12">
+            <div class="card">
+              <div class="card-body">
+                <h5 class="card-title">kontak</h5>
+                <p class="card-text">PT Pariwisata Lombok merupakan perusahaan yang bergerak dibidang pariwisata di wilayah Lombok. Pulau Lombok adalah sebuah pulau di kepulauan Sunda Kecil atau Nusa Tenggara yang terpisahkan oleh Selat Lombok dari Bali di sebelah barat dan Selat Alas di sebelah timur dari Sumbawa. Ada banyak tempat wisata yang harus dikunjungi di daerah lombok diantaranya yaitu, Air terjun Penimbung, Gunung Rinjani, Gua Bangka, Gili Terawang dan masih banyak lainnya.</p>
+                <p class="card-text">lombok@gmail.com</p>
+                <p class="card-text">085274929145</p>
+              </div>
+          </div>
+            </div>
+
+            <!--awal footer-->
+            <?php include('footer.php');?>
+                 
+
+
